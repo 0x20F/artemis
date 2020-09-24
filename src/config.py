46ff9410dd08a423d7eb11e72dc4448d96c2bd92
Poly <alexhexan22@gmail.com>
2020-09-24 17:13:03 +0200

    Start designing what needs doing

---
+++ src/config.py
@@ -0,0 +1,3 @@
+# Contains the parsing for the configuration file
+# that contains all sites that should be scraped
+# and how you should go about scraping those sites
--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ p_tags_text = [tag.get_text().strip() for tag in soup.select('article section p'
 
 
 # Filter out useless text
-sentence_list = [sentence for sentence in p_tags_text if not '\n' in sentence]
+sentence_list = [sentence for sentence in p_tags_text if '\n' not in sentence]
 sentence_list = [sentence for sentence in sentence_list if '.' in sentence]
 # Combine them all into a string
 article = ' '.join(sentence_list)
