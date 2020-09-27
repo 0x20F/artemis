7414d80466e2543c00367329c0898ecdeb5cc3c7
Poly <alexhexan22@gmail.com>
2020-09-27 14:05:47 +0200

    Understand how it actually works

--- src/db.py
+++ src/db.py
@@ -2,6 +2,10 @@ from pymongo import MongoClient
 from pprint import pprint
 
 client = MongoClient('mongodb://root:toor@localhost:27017')
-db = client.admin
-status = db.command('serverStatus')
-pprint(status)
+
+# Creates a new database called 'lmao' if it doesn't exist
+db = client.lmao
+
+# Creates a new collection called 'not_lmao' in
+# that database and inserts the object into it
+db.not_lmao.insert_one({ "aye": "lmao" })
